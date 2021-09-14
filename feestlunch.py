904190eeaa2e2948a2e9074ef37e9e5d5d0144c2
unknown <99067235@mydavinci.nl>
2021-09-14 09:53:44 +0200

    'opdracht_afgerond'

---
+++ feestlunch.py
@@ -0,0 +1,3 @@
+calculated = 17 * 39 + 2 * 278 - 3 * 50
+txt = "De feestlunch kost je bij de bakker {} cent voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+print(txt.format(calculated))
---
+++ speelhaldag.py
@@ -0,0 +1,3 @@
+calculated = 4 * 745 + 9 * 37 + 333 * 4
+txt = "Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {} cent"
+print(txt.format(calculated))
